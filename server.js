eeefc4b0d994e80d20267dd282b2941d032975d1
Jacob Adams <jacob.adams121202@gmail.com>
2025-04-14 17:14:33 -0700

    feat: update server.js to connect to MongoDB using Mongoose and add dotenv for environment variables

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const factsRoutes = require("./routes/facts");
 const PORT = process.env.PORT || 3000;
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 app.use(express.json());
-app.use("/api/facts", factsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
