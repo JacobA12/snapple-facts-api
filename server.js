0f3c46b4bc8bb8858429e006ee87078e68432a52
Jacob Adams <jacob.adams121202@gmail.com>
2025-04-14 17:19:31 -0700

    feat: refactor MongoDB connection logic in server.js for improved error handling and connection confirmation

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//client options provided by MongoDB
 const clientOptions = {
   serverApi: { version: "1", strict: true, deprecationErrors: true },
 };
 
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(process.env.MONGO_URI, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
+//connect to MongoDB database
+mongoose
+  .connect(process.env.MONGO_URI, clientOptions)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    //Start Express server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("mongoDB connection error:", err);
+  });
 
 app.use(express.json());
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
